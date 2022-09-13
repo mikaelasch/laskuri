663d7e3b4686936f2b4b084d4f8438e7a2a68f01
Mikaela Schröder <mikaela.schroder@myy.haaga-helia.fi>
2022-09-13 19:05:37 +0300

    First commit

--- App.js
+++ App.js
@@ -1,43 +1,59 @@
 import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View, Pressable, FlatList } from 'react-native';
 
 export default function App() {
 
-  const [result, setResult] = useState(0);
-  const[number1, onChangeNumber1] = useState(null);
-  const[number, onChangeNumber] = useState(null)
-  const handleAdd = () => setResult(Number(number) + Number(number1))
-  const handleSubtract = () => setResult(Number(number) - Number(number1))
+  const[result, setResult] = useState(0)
+  const[firstNumber, setFirstNumber] = useState(null)
+  const[secondNumber, setSecondNumber] = useState(null)
+  const[data, setData] = useState([])
+ 
+  const handleAdd = () => {
+  
+  let total = Number(firstNumber) + Number(secondNumber)
+  setResult(Number(firstNumber) + Number(secondNumber))
+  setData([...data, {key: firstNumber + " + " + secondNumber + " = " + total}])
+  }
+  
+  const handleSubtract = () => {
+  
+  let total = Number(firstNumber) - Number(secondNumber)
+  setResult(Number(firstNumber) - Number(secondNumber))
+  setData([...data, {key: firstNumber + " - " + secondNumber + " = " + total}])
+  }
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
       <TextInput
-      style={{ width:200, borderColor:'gray' , borderWidth: 1}}
-      onChangeText={number => onChangeNumber(number)}
-      value={number}
-      keyboardType="numeric"
-      />
+            style={{ width:200, borderColor:'gray' , borderWidth: 1}}
+            onChangeText={firstNumber => setFirstNumber(firstNumber)}
+            value={firstNumber}
+            keyboardType="numeric"/>
       <TextInput
-      style={{ width:200, borderColor:'gray' , borderWidth: 1}}
-      onChangeText={number1 => onChangeNumber1(number1)}
-      value={number1}
-      keyboardType="numeric"
-      
-      />
-      <View style={styles.buttonContainer}>
+            style={{ width:200, borderColor:'gray' , borderWidth: 1}}
+            onChangeText={secondNumber => setSecondNumber(secondNumber)}
+            value={secondNumber}
+            keyboardType="numeric" />
+   <View style={styles.buttonContainer}>
       <Pressable
-       style={styles.button}
-      onPress={handleAdd}>
-        <Text style={styles.button}>+</Text>
-        </Pressable> 
+            style={styles.button}
+            onPress={handleAdd}>
+      <Text style={styles.button}>+</Text>
+      </Pressable> 
       <Pressable 
-      style={styles.button}
-       onPress={handleSubtract}>
+            style={styles.button}
+            onPress={handleSubtract}>
       <Text style={styles.button}>-</Text>
       </Pressable>
-      </View>
+   </View>
+   <View style={styles.list}>
+      <FlatList
+      data={data}
+      renderItem={({item}) => <Text>{item.key}</Text> }
+      />
+  </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -45,12 +61,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
     button: {
+      flex:1.5,
       backgroundColor: 'blue',  
       alignItems: 'center',
       color: 'black',
@@ -59,10 +76,14 @@ const styles = StyleSheet.create({
 
   },
   buttonContainer: {
-    
+    flex: 0.1,
     flexDirection: "row",
     justifyContent:'space-around',
-    padding:'3%',
+    padding:'12%',
     width: '40%'
-  }
+  },
+  list: {
+    flex: 0.7,
+
+  },
 });
